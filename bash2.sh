6a3e515142167be17d8a70b0566853c1d6c3772a
Mumtaz Fatima <shereenfatima1000@gmail.com>
2025-12-12 00:00:44 +0100

    Bash2 complete

---
+++ bash2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+echo "Hej vad heter du?";
+read name;
+echo "Hur gammal är du?";
+read age;
+echo "Vilken ort bor du?";
+read city;
+echo "Hej $name, du är $age år och bor i $city."
