0fd48fc39de6150aaae6399378aa634b5ec2d332
Mumtaz Fatima <shereenfatima1000@gmail.com>
2025-12-22 00:23:09 +0100

    Bash 4 complete

--- bash4.sh
+++ bash4.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/bash
-current_sec=$(date +%S);
-echo "Current seconds is $current_sec";
-stop_sec=$((current_sec + 5));
-echo "Stop second is $stop_sec";
-while [ $(date +%S) -le "$stop_sec" ]
-do
-        echo "Current sec is $(date +%S)";
-done
-echo "Loop complete";
+#!/bin/bash
+read -t 5 -p "Vad är din favoritfärg? " color
+
+if [ -z "$color" ]; then
+    color="röd"
+fi
+
+echo "Din favoritfärg är $color."
