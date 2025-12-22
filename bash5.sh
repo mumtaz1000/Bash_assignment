fb05bf6cea905021f1284ae6ddada7e4d8969446
Mumtaz Fatima <shereenfatima1000@gmail.com>
2025-12-22 01:15:31 +0100

    Bash 5 code complete

---
+++ bash5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "Hej! kan du skriva första tal" first_number;
+read -p "Hej! kan du skriva andra tal" second_number; 
+if [[ $first_number -eq $second_number ]]; then
+      echo "Talen är lika"
+else 
+      echo "Talen är inte lika"
+      if [[ $first_number -gt $second_number ]]; then
+            echo "Första tal är större än andra"
+      else
+            echo "Första tal är mindre än andra"
+      fi
+fi
