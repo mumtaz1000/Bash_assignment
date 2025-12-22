4bf4395af809d9eea935bd4f2a30304c33f72191
Mumtaz Fatima <shereenfatima1000@gmail.com>
2025-12-23 00:08:46 +0100

    Bash 8 complete

--- bash8.sh
+++ bash8.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 read -p "Skriv in siffran för att få betyg. " grade;
 echo -n "Du är "
-case $grade in 
+if [[ $grade -lt 50 ]]; then
+      echo "IG";
+elif [[ $grade -ge 50 && $grade -lt 65 ]]; then
+      echo "G";
+elif [[ $grade -ge 65 && $grade -lt 90 ]]; then
+      echo "VG";
+elif [[ $grade -ge 90 ]]; then
+      echo "MVG";
+else 
+      echo "Fel!!!!!!"
+fi
+
