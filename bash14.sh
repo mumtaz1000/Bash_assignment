f1f6f39798da23804d6e1d5855e8265c1057b6bd
Mumtaz Fatima <shereenfatima1000@gmail.com>
2025-12-26 11:04:29 +0100

    Bash 14 complete

--- bash14.sh
+++ bash14.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/bash
+home_directory=$HOME;
+user_name=$(whoami);
+folder_name=$(date +%d%m%Y_%H%M%S)
+backup_directory="/tmp/$user_name.$folder_name.tar.gz";
+
+tar -czf "$backup_directory" "$home_directory";
+
+if [ $? -eq 0 ]; then 
+      echo "Huraaa!!!! Lyckas att skapa backup.."
+else 
+      echo "Fel! Inte lyckas att skapa backup"
+fi
